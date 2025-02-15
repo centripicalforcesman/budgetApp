b40c4ecc500dee28fe315ba3e676bc558bcf8209
Chuck Watson <chuckwatson87@gmail.com>
2025-02-15 12:24:22 -0500

    setting proper folder structure for data layer

---
+++ budgets/services/budgets/data/models/budget.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, DateTime, String, Integer, func
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+metadata = Base.metadata
+
+class Budget(Base):
+    __tablename__ = "budget"
+
+    id: Mapped[int] =  mapped_column(primary_key=True)
+    month: Mapped[int] = mapped_column(nullable=False)#will eventually be enum for month
+    year: Mapped[int] = mapped_column(nullable=False)
+
---
+++ budgets/services/budgets/data/models/budgetGroup.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, DateTime, Integer, func, ForeignKey, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import relationship
+
+
+Base = declarative_base()
+metadata = Base.metadata
+
+class BudgetGroup(Base):
+    __tablename__ = "budgetGroup"
+
+    id: Mapped[int] =  mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+
