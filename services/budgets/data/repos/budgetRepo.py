63b9b643effadb66f39aaf262a2d201b1a21964a
Chuck Watson <chuckwatson87@gmail.com>
2025-07-05 12:15:48 -0400

    moving engine sqlalchemy related code to data layer

--- services/budgets/data/repos/budgetRepo.py
+++ services/budgets/data/repos/budgetRepo.py
@@ -1,18 +1,28 @@
+from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from services.budgets.data.models.budget import Budget
 from services.budgets.mappers.budgetMappers import budget_data_to_budget_domain
+from services.budgets.data.database import Base
+
+engine  = create_engine(
+    "postgresql://postgres:password@localhost/budget_db",
+    echo=True)
+
+Base.metadata.create_all(engine)
 
 class budgetRepo:
-    def __init__(self, session: Session, identity_map=None):
-        self.session = session
+    def __init__(self, identity_map=None):
         self.__identity_map = identity_map or dict()
     
     def get_by_id(self,id):
-        budget = self.session.query(Budget).get(id)
-        return budget_data_to_budget_domain(budget)
+        with Session(engine) as session:
+            budget = session.query(Budget).get(id)
+            return budget_data_to_budget_domain(budget)
     
     def get_by_year_month(self, year, month):
-        budget = self.session.query(Budget).filter(Budget.year == year, Budget.month == month).one()
-        return budget_data_to_budget_domain(budget)
+        with Session(engine)as session:
+            budget = session.query(Budget).filter(Budget.year == year, Budget.month == month).one()
+            return budget_data_to_budget_domain(budget)
+    
         
 
--- services/budgets/operations/GetBudget.py
+++ services/budgets/operations/GetBudget.py
@@ -1,16 +1,6 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
 from services.budgets.data.repos.budgetRepo import budgetRepo
-from services.budgets.data.database import Base
 
-#TODO: move to data layer
-engine  = create_engine(
-    "postgresql://postgres:password@localhost/budget_db",
-    echo=True)
-
-Base.metadata.create_all(engine)
 class GetBudget: 
     def Execute(self, id):
-        with Session(engine) as session:
-            repo = budgetRepo(session, [])
-            return repo.get_by_id(id).serialize()
+        repo = budgetRepo([])
+        return repo.get_by_id(id).serialize()
